c23d1114488531d64fe64c480761fd5237091b42
Cesar Lai <git@cesarlai.com>
2021-09-10 13:20:42 +0800

    feat: add plugins and lints

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 90,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  semi: false,
+  jsxSingleQuote: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid'
+}
